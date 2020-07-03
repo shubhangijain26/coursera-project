2137f0190dc612cd677ecb9e61449a91089c667a
shubhangi <shubhangi.jain@industrybuying.com>
2020-07-03 09:17:00 +0530

    SPA

--- foodApp/src/app/home/home.component.ts
+++ foodApp/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/menu';
+import { DishService } from '../services/dish.service';
+import { PromotionService } from '../services/promotion.service';
+import { Promotion } from '../shared/promotion';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  dish :Dish;
+  promotion: Promotion;
+  
+  constructor(private dishService: DishService, private promoService: PromotionService) { }
 
   ngOnInit() {
+   this.dish = this.dishService.getFeaturedDish();
+   this.promotion = this.promoService.getFeaturedPromotion();
   }
 
 }
